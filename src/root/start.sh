05cc9017c874fd4b59c1334609340462e780dbd0
craig <craig@nykindale.com>
2024-01-27 20:58:35 +0000

    Removed isync.

--- src/root/start.sh
+++ src/root/start.sh
@@ -5,12 +5,4 @@ mkdir -p /home/app/.mail/${NAME}
 chown -R 1000:1000 /home/app
 find /home/app -type d -print0 | xargs -0 chmod 755
 
-sed -i "s/{NAME}/${NAME}/g" /mbsync.conf
-sed -i "s/{SERVER}/${SERVER}/g" /mbsync.conf
-sed -i "s/{EMAIL}/${EMAIL}/g" /mbsync.conf
-sed -i "s/{PASSWORD}/${PASSWORD}/g" /mbsync.conf
-sed -i "s/{NAME}/${NAME}/g" /isync.cron
-
-su -c 'crontab /isync.cron' app
-
 /usr/sbin/dovecot -F

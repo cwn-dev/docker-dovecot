92e5fbc8941b75940a8149f8555ab75b3bd629bc
craig <craig@nykindale.com>
2024-01-27 11:17:21 +0000

    Addd cron job for isync

--- src/root/start.sh
+++ src/root/start.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-mkdir -p /home/app/.mail/runbox
+mkdir -p /home/app/.mail/${NAME}
 
-chown -R 1000:1000 /home/app
-find /home/app -type d -print0 | xargs -0 chmod 755
+#chown -R 1000:1000 /home/app
+#find /home/app -type d -print0 | xargs -0 chmod 755
 
 sed -i "s/{NAME}/${NAME}/g" /mbsync.conf
 sed -i "s/{SERVER}/${SERVER}/g" /mbsync.conf
 sed -i "s/{EMAIL}/${EMAIL}/g" /mbsync.conf
 sed -i "s/{PASSWORD}/${PASSWORD}/g" /mbsync.conf
-sed -i "s/{NAME}/${NAME}/g" /etc/cron.d/isync
+sed -i "s/{NAME}/${NAME}/g" /isync.cron
+
+su -c 'crontab /isync.cron' app
 
 /usr/sbin/dovecot -F

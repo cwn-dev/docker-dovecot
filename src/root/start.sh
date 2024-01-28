13424be2e59ecb2f677eef8894160b80842f26b0
craig <craig@nykindale.com>
2024-01-28 21:52:55 +0000

    Added dovecot-submissiond so mail can be relayed through dovecot to external SMTP.

--- src/root/start.sh
+++ src/root/start.sh
@@ -5,4 +5,9 @@ mkdir -p /home/app/.mail/${NAME}
 chown -R 1000:1000 /home/app
 find /home/app -type d -print0 | xargs -0 chmod 755
 
+sed -i "s/{SMTP_HOST}/${SMTP_HOST}/g" /etc/dovecot/conf.d/10-submission.conf
+sed -i "s/{SMTP_PORT}/${SMTP_PORT}/g" /etc/dovecot/conf.d/10-submission.conf
+sed -i "s/{SMTP_USER}/${SMTP_USER}/g" /etc/dovecot/conf.d/10-submission.conf
+sed -i "s/{SMTP_PASSWORD}/${SMTP_PASSWORD}/g" /etc/dovecot/conf.d/10-submission.conf
+
 /usr/sbin/dovecot -F
